e4ee09e821c702257d12b04e8d9eb2db331faeb6
alex_shatailov <alexandshatailov@gmail.com>
2021-11-09 16:35:40 +0200

    add simple profile

--- src/views/ProfilePage.js
+++ src/views/ProfilePage.js
@@ -1,8 +1,15 @@
+import { Col, Container, Row } from "reactstrap"
+
 const ProfilePage = () => {
     return(
-        <div>
-            profile
-        </div>
+        <Container>
+            <Row>
+                <h1>Profile</h1>
+                <Col md={10} className>
+                   
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
