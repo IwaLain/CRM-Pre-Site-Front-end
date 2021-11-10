dbd61f2dcbc79774a4646d07b66136ad552923b5
alex_shatailov <alexandshatailov@gmail.com>
2021-11-10 11:36:01 +0200

    add simple profile

--- src/views/ProfilePage.js
+++ src/views/ProfilePage.js
@@ -1,14 +1,53 @@
-import { Col, Container, Row } from "reactstrap"
-
+import { Col, Container, Label, Row } from "reactstrap"
+import '../scss/components/profilepage.scss'
 const ProfilePage = () => {
     return(
         <Container>
+            <h3>Profile</h3>
             <Row>
-                <h1>Profile</h1>
-                <Col md={10} className>
-                   
+                <Col lg={12} className='p-5 pt-4'>
+                    <Row className='profile__item'>
+                        <Col className='align-self-center' sm={6} lg={3}>
+                            <Label className='profile__label'>Avatar</Label>
+                        </Col>
+                        <Col sm={6} lg={3} className='d-flex justify-content-center'>
+                            <img className='profile__img' src="" alt="Avatar" />
+                        </Col>
+                    </Row>
+                    <Row className='profile__item mt-3'>
+                        <Col lg={3}>
+                            <Label className='profile__label'>Username</Label>
+                        </Col>
+                        <Col lg={3}>
+                            <p className='profile__text'>Alakey123</p>
+                        </Col>
+                    </Row>
+                    <Row className='profile__item'>
+                        <Col lg={3}>
+                            <Label className='profile__label'>First Name</Label>
+                        </Col>
+                        <Col lg={3}>
+                            <p className='profile__text'>Alex</p>
+                        </Col>
+                    </Row>
+                    <Row className='profile__item'>
+                        <Col lg={3}>
+                            <Label className='profile__label'>Last Name</Label>
+                        </Col>
+                        <Col lg={3}>
+                            <p className='profile__text'>Shatailov</p>
+                        </Col>
+                    </Row>
+                    <Row className='profile__item'>
+                        <Col lg={3}>
+                            <Label className='profile__label'>Email Adress</Label>
+                        </Col>
+                        <Col lg={3}>
+                            <p className='profile__text'>samoran232@gmail.com</p>
+                        </Col>
+                    </Row>
                 </Col>
-            </Row>
+            </Row>           
         </Container>
     )
 }
