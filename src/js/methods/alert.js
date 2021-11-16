61b099323e85ea67fa1abecab73cf4258aee264b
Пользователь <arthes.menetil@gmail.com>
2021-11-16 14:02:24 +0200

    refactoring js methods

--- src/js/methods/alert.js
+++ src/js/methods/alert.js
@@ -1,23 +1,23 @@
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 export const alert = (type, message) => {
-    let notify = () => {
-        switch (type) {
-            case 'success':
-                toast.success(message)
-                break
-            case 'error':
-                toast.error(message)
-                break
-            case 'warning':
-                toast.warn(message)
-                break
-            case 'info':
-                toast.info(message)
-                break
-            default:
-                break
-        }
+  let notify = () => {
+    switch (type) {
+      case "success":
+        toast.success(message);
+        break;
+      case "error":
+        toast.error(message);
+        break;
+      case "warning":
+        toast.warn(message);
+        break;
+      case "info":
+        toast.info(message);
+        break;
+      default:
+        break;
     }
-    return notify()
-}
+  };
+  return notify();
+};
--- src/js/methods/validation.js
+++ src/js/methods/validation.js
@@ -1,18 +1,18 @@
 export const validation = (type, data) => {
-    switch (type) {
-        case 'username':
-            break
-        case 'password':
-            break
-        case 'email':
-            break
-        case 'phone':
-            break
-        case 'role':
-            break
-        case 'text':
-            break
-        default:
-            break
-    }
-}
+  switch (type) {
+    case "username":
+      break;
+    case "password":
+      break;
+    case "email":
+      break;
+    case "phone":
+      break;
+    case "role":
+      break;
+    case "text":
+      break;
+    default:
+      break;
+  }
+};
