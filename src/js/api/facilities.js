01ab8bd19570cf30edf42657f7663b2c423405ec
Пользователь <arthes.menetil@gmail.com>
2021-11-26 12:21:41 +0200

    fixed merge conflicts

--- src/js/api/facilities.js
+++ src/js/api/facilities.js
@@ -1,66 +1,6 @@
-import { api } from "./api"
+import { api } from "./api";
 
-<<<<<<< HEAD
 const token = localStorage.getItem("token");
-=======
-const token = localStorage.getItem('token')
->>>>>>> 46d90e9fb9e1c2f511625f5d41cde3ae94609dc7
-
-const getFacilities = async () => {
-    let config = {
-        method: 'GET',
-        url: `/api/facilities?access-token=${token}`
-    }
-
-    const response = await api(config)
-    return response.Facilities
-}
-
-const getFacilityApi = async (FacilitiesId) => {
-    let config = {
-        method: 'GET',
-        url: `/api/facilities/${FacilitiesId}?access-token=${token}`
-    }
-
-    const response = await api(config)
-    return response.Facilities
-}
-
-const addFacilities = async (data) => {
-    let config = {
-        method: 'POST',
-        url: `/api/facilities/create?access-token=${token}`,
-        data: data
-    } 
-
-    const response = await api(config)
-    return response
-}
-
-const editFacilities = async (FacilitiesId, data) => {
-    let config = {
-        method: 'PUT',
-        url: `/api/facilities/update/${FacilitiesId}?access-token=${token}`,
-        data: data
-    }
-
-    const response = await api(config)
-    return response
-}
-
-const deleteFacilities = async (FacilitiesId) => {
-    let config = {
-        method: 'DELETE',
-        url: `/api/facilities/delete/${FacilitiesId}?access-token=${token}`
-    }
-
-    const response = await api(config)
-    return response
-}
-
-<<<<<<< HEAD
-  return response.facility[facilityId];
-};
 
 const getFacilities = async () => {
   let config = {
@@ -69,7 +9,48 @@ const getFacilities = async () => {
   };
 
   const response = await api(config);
+  return response.Facilities;
+};
 
+const getFacilityApi = async (FacilitiesId) => {
+  let config = {
+    method: "GET",
+    url: `/api/facilities/${FacilitiesId}?access-token=${token}`,
+  };
+
+  const response = await api(config);
+  return response.Facilities;
+};
+
+const addFacilities = async (data) => {
+  let config = {
+    method: "POST",
+    url: `/api/facilities/create?access-token=${token}`,
+    data: data,
+  };
+
+  const response = await api(config);
+  return response;
+};
+
+const editFacilities = async (FacilitiesId, data) => {
+  let config = {
+    method: "PUT",
+    url: `/api/facilities/update/${FacilitiesId}?access-token=${token}`,
+    data: data,
+  };
+
+  const response = await api(config);
+  return response;
+};
+
+const deleteFacilities = async (FacilitiesId) => {
+  let config = {
+    method: "DELETE",
+    url: `/api/facilities/delete/${FacilitiesId}?access-token=${token}`,
+  };
+
+  const response = await api(config);
   return response;
 };
 
@@ -84,8 +65,6 @@ const createFacility = async (data) => {
   return response;
 };
 
-=======
->>>>>>> 46d90e9fb9e1c2f511625f5d41cde3ae94609dc7
 const addFacilityImageApi = async (facilityId, dataImg) => {
   let config = {
     method: "POST",
@@ -128,14 +107,10 @@ const updateFacilityAPI = async (facilityId, dataFacility) => {
 export {
   getFacilities,
   getFacilityApi,
-<<<<<<< HEAD
-  getFacilities,
   createFacility,
-=======
   addFacilities,
   editFacilities,
   deleteFacilities,
->>>>>>> 46d90e9fb9e1c2f511625f5d41cde3ae94609dc7
   addFacilityImageApi,
   deleteFacilityImageAPI,
   setMainFacilityImageAPI,
--- src/js/methods/validation.js
+++ src/js/methods/validation.js
@@ -1,12 +1,11 @@
 const regex = {
-<<<<<<< HEAD
   password: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
   email:
     /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
   phone: /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/,
 };
 
-const validation = (type) => {
+export const validation = (type) => {
   switch (type) {
     case "username":
       return {
@@ -76,80 +75,3 @@ const validation = (type) => {
       break;
   }
 };
-export { validation };
-=======
-    password: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
-    email: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-    phone: /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/,
-}
-
-export const validation = (type) => {
-    switch (type) {
-        case 'username':
-            return {
-                required: "UserName is Required",
-                minLength: {
-                    value: 3,
-                    message: "Minimum 3 simvols",
-                }
-            }
-        case 'password':
-            return {
-                required: "Password is Required",
-                minLength: {
-                    value: 4,
-                    message: "Minimum 4 simvols",
-                },
-                maxLength: {
-                    value: 16,
-                    message: "Maximum 16 simvols",
-                },
-                pattern: {
-                    value: regex.password,
-                    message: "Password should contain atleast one number and one special character",
-                }
-            }
-        case 'email':
-            return {
-                required: "Email is Required",
-                minLength: {
-                    value: 5,
-                    message: "Minimum 5 simvols",
-                },
-                pattern: {
-                    value: regex.email,
-                    message: "Email must be like exampl@mail.com",
-                }
-            }
-        case 'phone':
-            return {
-                required: "Phone is Required",
-                minLength: {
-                    value: 9,
-                    message: "Minimum 9 simvols",
-                },
-                pattern: {
-                    value: regex.phone,
-                    message: "Phone must be like 555-555-5555",
-                }
-            }
-        case 'role':
-            return {
-                pattern: {
-                    value: 'Select role',
-                    message: 'Please select role'
-                }
-            }
-        case 'text':
-            return {
-                required: 'This input is required',
-                minLength: {
-                    value: 4,
-                    message: "Minimum 4 simvols",
-                },
-            }
-        default:
-            break
-    }
-}
->>>>>>> 46d90e9fb9e1c2f511625f5d41cde3ae94609dc7
