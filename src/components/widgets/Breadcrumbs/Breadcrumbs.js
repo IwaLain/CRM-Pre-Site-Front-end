24420442e7d483a8ad03c9f376271f99991cc5f6
Пользователь <arthes.menetil@gmail.com>
2021-11-26 11:38:59 +0200

    fixed breadcrumbs

--- src/components/widgets/Breadcrumbs/Breadcrumbs.js
+++ src/components/widgets/Breadcrumbs/Breadcrumbs.js
@@ -2,10 +2,13 @@ import withBreadcrumbs from "react-router-breadcrumbs-hoc";
 import { Link } from "react-router-dom";
 import "../../../scss/breadcrumbs.scss";
 import { getCustomerAPI } from "../../../js/api/customer";
+import { getFacilityApi } from "../../../js/api/facilities";
+import { getLocationAPI } from "../../../js/api/locations";
+import { getEquipment } from "../../../js/api/equipment";
 import { useState } from "react";
 
 const Breadcrumbs = ({ breadcrumbs }) => {
-  const [customerName, setCustomerName] = useState();
+  const [name, setName] = useState();
 
   return (
     <nav className="breadcrumbs">
@@ -16,18 +19,35 @@ const Breadcrumbs = ({ breadcrumbs }) => {
             parseInt(breadcrumb.props.children)
           ) {
             getCustomerAPI(breadcrumb.props.children).then((customer) => {
-              setCustomerName(customer.name);
+              setName(customer.name);
+            });
+          } else if (
+            breadcrumb.key.includes("/dashboard/facilities/") &&
+            parseInt(breadcrumb.props.children)
+          ) {
+            getFacilityApi(breadcrumb.props.children).then((facility) => {
+              setName(facility.name);
+            });
+          } else if (
+            breadcrumb.key.includes("/dashboard/locations/") &&
+            parseInt(breadcrumb.props.children)
+          ) {
+            getLocationAPI(breadcrumb.props.children).then((location) => {
+              setName(location.name);
+            });
+          } else if (
+            breadcrumb.key.includes("/dashboard/equipment/") &&
+            parseInt(breadcrumb.props.children)
+          ) {
+            getEquipment(breadcrumb.props.children).then((equipment) => {
+              setName(equipment.name);
             });
           }
 
           return (
             <div key={match.url} className="breadcrumbs__breadcrumb-item">
               <Link to={match.url || ""}>
-                {breadcrumb.key.includes(
-                  "/dashboard/customers/" && parseInt(breadcrumb.props.children)
-                )
-                  ? customerName
-                  : breadcrumb}
+                {parseInt(breadcrumb.props.children) ? name : breadcrumb}
               </Link>
               {index < breadcrumbs.length - 1 && (
                 <span className="breadcrumbs__divider">-</span>
